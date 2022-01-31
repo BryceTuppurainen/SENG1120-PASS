4684e838b479349e0e9a8ae1af14f37517f5b25d
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-01-31 15:34:19 +1100

    Added helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,24 @@
+/*
+
+1.0.0 - Check https://semver.org/
+
+Author: Bryce Tuppurainen Jan-2022
+
+This script when compiled and executed outputs the text "Hello World!" on the
+CLI
+
+Note, the version, author, date, and a brief description of the scripts
+functionality as a comment at the top of your files is highly recommended
+
+Also, check out Clang-Format, Prettier, or a similar formatting tool in order to
+make tidying up your scripts much easier when working on large projects or group
+projects
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
